4e32af491998562acff8382fedf32d75ed3ce739
Amarnath1810 <veeramreddyamarnathreddy@gmail.com>
2026-08-05 15:48:58 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,20 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://amarnathreddy.vercel.app";
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://amarnathreddy.vercel.app/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
